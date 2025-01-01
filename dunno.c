386addb0057f4cd905c55b931d35db84a303054e
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-01 21:54:34 +0000

    preparing for ast parsing

--- dunno.c
+++ dunno.c
@@ -2,6 +2,17 @@
 // Not using C standard headers to play almost like a quine. In theory, we can
 // make the compiler eventually generate a code like this, whilst making the
 // language expressive enough for the task.
+
+typedef long size_t;
+
+typedef enum token_t {
+  t_nil,
+  t_idkw,
+  t_ignore,
+  t_punct,
+  t_string,
+} token_t;
+
 extern int    close(int);
 extern void   exit (int);
 extern int    open (const char *, int);
@@ -13,6 +24,7 @@ int g_fd;
 char g_peek = 0;
 char g_took = 0;
 char g_tok[10240];
+token_t g_tok_t;
 int g_line = 1;
 int g_col = 0;
 
@@ -26,7 +38,7 @@ void write_num(int n) {
   char b = '0' + (n % 10);
   write(2, &b, 1);
 }
-void fail(const char * msg) {
+[[noreturn]] void fail(const char * msg) {
   write(2, g_file, len(g_file));
   write(2, ":", 1);
   write_num(g_line);
@@ -75,12 +87,16 @@ int is_punct(char c) {
   return c == '{' || c == '}' || c == '(' || c == ')' || c == ';';
 }
 
-int ignore() {
+token_t eof() {
   g_tok[0] = 0;
-  return 1;
+  return g_tok_t = t_nil;
+}
+token_t ignore() {
+  g_tok[0] = 0;
+  return g_tok_t = t_ignore;
 }
 
-int take_id_or_kw_token() {
+token_t take_id_or_kw_token() {
   char * c = g_tok;
   while (g_took) {
     *c++ = g_took;
@@ -88,10 +104,11 @@ int take_id_or_kw_token() {
     take_char();
   }
   *c = 0;
-  return 1;
+  g_tok_t = t_idkw;
+  return t_idkw;
 }
 
-int take_string() {
+token_t take_string() {
   char * c = g_tok;
   while (take_char() != '"' && g_took) {
     // TODO: actual escaping;
@@ -101,22 +118,24 @@ int take_string() {
   if (!g_took) fail("unclosed string");
 
   *c = 0;
-  return 1;
+  g_tok_t = t_string;
+  return t_string;
 }
 
-int take_space() {
+token_t take_space() {
   while (is_space(peek_char()) && g_took) take_char();
   return ignore();
 }
 
-int take_punct() {
+token_t take_punct() {
   g_tok[0] = g_took;
   g_tok[1] = 0;
-  return 1;
+  g_tok_t = t_punct;
+  return t_punct;
 }
 
-int take_token() {
-  if (take_char() == 0) return 0;
+token_t take_token() {
+  if (take_char() == 0) return eof();
   if (is_idkw_start(g_took)) return take_id_or_kw_token();
   if (is_space(g_took)) return take_space();
   if (g_took == ';') return ignore();
@@ -124,15 +143,28 @@ int take_token() {
   if (g_took == '"') return take_string();
 
   fail("invalid token");
-  return 0;
+}
+
+token_t take_valid_token() {
+  while (take_token() == t_ignore) {}
+  return g_tok_t;
+}
+
+int top_level() {
+  switch (take_valid_token()) {
+    default: fail("unexpected token");
+  }
 }
 
 int main() {
   g_fd = open(g_file, 0);
   if (g_fd < 0) fail("file not found");
 
-  while (take_token()) {
+  while (take_valid_token()) {
+    write_num(g_tok_t);
+    write(1, " ", 1);
     write(1, g_tok, len(g_tok));
+    write(1, "\n", 1);
   }
 
   close(g_fd);

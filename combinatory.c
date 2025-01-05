d64ea245cf1129bc18c97d2101820b2576399dff
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-05 18:04:00 +0000

    making this bottom-up is making more sense

--- combinatory.c
+++ combinatory.c
@@ -91,6 +91,7 @@ void slurp(const char * file) {
 
 typedef enum type {
   a_err,
+  a_ident,
 } type_t;
 typedef struct ast {
   int j;
@@ -105,16 +106,74 @@ ast_t err(int j, const char * msg) {
 }
 
 ast_t empty(int j) { return (ast_t) { j }; }
-ast_t term(int j, char c) {
-  if (j >= g_len) return err(j, "unexpected eof");
-  if (g_buf[j] == c) return (ast_t) { .j = j + 1 };
-  return err(j, "mismatched char");
-}
 
+ast_t alt(int j, parser_t * p) {
+  ast_t r = { -1 };
+  while (*p) {
+    ast_t r2 = (*p++)(j);
+    if (r2.j >= 0 && r.j >= 0) fail(j, "grammar is ambiguous for this");
+    if (r2.j >= 0) r = r2;
+  }
+  if (r.j == -1) return err(j, "no valid alternative");
+  return r;
+}
 ast_t seq(int j, parser_t * p) {
   ast_t r = { j };
   while (*p && r.j >= 0) r = (*p++)(r.j);
   return r;
+}
+
+ast_t term(int j, int (*fn)(char)) {
+  return fn(g_buf[j]) ? (ast_t) { j + 1, j } : err(j, "mismatched char");
+}
+ast_t whilst(int j, int (*fn)(char)) {
+  ast_t res = { .start_j = j };
+  while (fn(g_buf[j])) j++;
+  res.j = j;
+  return res;
+}
+
+int cc_alpha(char c) { c |= 0x20; return c >= 'a' && c <= 'z'; }
+int cc_digit(char c) { return c >= '0' && c <= '9'; }
+int cc_eol(char c) { return c == 0 || c == '\n'; }
+int cc_ident(char c) { return cc_alpha(c) || cc_digit(c) || c == '_'; }
+int cc_non_eol(char c) { return !cc_eol(c); }
+int cc_non_ident(char c) { return !cc_ident(c); }
+int cc_space(char c) { return c == ' ' || c == '\t' || c == '\r' || c == '\n'; }
+
+ast_t str(int j, const char * str) {
+  ast_t res = {
+    .start_j = j,
+    .type = a_ident,
+    .str = str,
+  };
+  while (g_buf[j] && *str && *str == g_buf[j]) {
+    j++;
+    str++;
+  }
+  if (*str) return err(res.start_j, "mismatched string");
+  res.j = j;
+  return res;
+}
+ast_t ident(int j, const char * s) {
+  ast_t res = str(j, s);
+  if (cc_ident(g_buf[res.j])) return err(j, "mismatched identifier");
+  return res;
+}
+ast_t i_int (int j) { return ident(j, "int" ); }
+ast_t i_void(int j) { return ident(j, "void"); }
+
+ast_t space(int j) { return term(j, cc_space); }
+ast_t comment_start(int j) { return str(j, "//"); }
+ast_t until_eol(int j) { return whilst(j, cc_non_eol); }
+ast_t comment(int j) { return seq(j, (parser_t[]) { comment_start, until_eol, 0 }); }
+ast_t space_or_comment(int j) { return alt(j, (parser_t[]) { comment, space, 0 }); }
+ast_t sp(int j) {
+  while (1) {
+    ast_t res = space_or_comment(j);
+    if (res.j == -1) return empty(j);
+    j = res.j;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -127,12 +186,8 @@ ast_t run(parser_t p, const char * code) {
   return p(0);
 }
 
-ast_t c_i(int j) { return term(j, 'i'); }
-ast_t c_n(int j) { return term(j, 'n'); }
-ast_t c_t(int j) { return term(j, 't'); }
-
 ast_t test(int j) {
-  return seq(j, (parser_t[]) { c_i, c_n, c_t, 0 });
+  return sp(j);
 }
 
 int main() {
